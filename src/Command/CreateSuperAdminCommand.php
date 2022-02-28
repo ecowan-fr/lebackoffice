cf9025f8ee1d2bb447fa8c06dbb081256c0f641f
Tydoo Bauer <thomas@tydoo.fr>
2022-02-28 14:56:48 +0100

    ADD setup command et EDIT create admin command

--- src/Command/CreateSuperAdminCommand.php
+++ src/Command/CreateSuperAdminCommand.php
@@ -6,20 +6,22 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(
-    name: 'app:create-user',
-    description: "Ajout d'un super-administrateur",
+    name: 'app:create-super-admin',
+    description: "Ajout d'un super administrateur",
 )]
-class CreateUserCommand extends Command {
+class CreateSuperAdminCommand extends Command {
 
     public function __construct(
         private readonly EntityManagerInterface $em,
@@ -34,28 +36,36 @@ class CreateUserCommand extends Command {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int {
-
         $io = new SymfonyStyle($input, $output);
 
-        //Checker automatiquement si la base de donnée existe et si toute les migrations ont été jouées
-        $io->caution("Attention, avant de continuer, il faut que la base de données soit déployée et que les migrations soient jouées.");
+        $io->writeln(''); //Ajout d'un retour a la ligne
+        $io->title("Ajout d'un super administrateur");
 
-        if (!$io->confirm("Voulez-vous continuer ?")) {
-            return Command::INVALID;
+        $NullOutput = new NullOutput;
+
+        //Chec automatique si la base de donnée existe et si toute les migrations ont été jouées
+        $command = $this->getApplication()->find('doctrine:migrations:up-to-date');
+        if ($command->run($input, $NullOutput) != 0) {
+            throw new RuntimeException(
+                "Toutes les migrations ne sont pas jouées.
+                Exécutez \"php bin/console doctrine:migrations:migrate\"
+                avant de relancer la création d'un super administrateur"
+            );
+            return Command::FAILURE;
         }
 
         $questionEmail = new Question('Email du compte super-administrateur');
         $questionEmail->setValidator(function ($answer) {
             if (!is_string($answer) || trim($answer) == '' || empty($answer)) {
-                throw new \RuntimeException(
+                throw new RuntimeException(
                     "L'adresse email est obligatoire"
                 );
             } elseif (count($this->validator->validate($answer, new EmailConstraint())) > 0) {
-                throw new \RuntimeException(
+                throw new RuntimeException(
                     "Ce n'est pas une adresse email valide"
                 );
             } elseif ($this->userRepository->count(['email' => $answer]) > 0) {
-                throw new \RuntimeException(
+                throw new RuntimeException(
                     "Un compte existe déjà avec cette adresse email"
                 );
             }
@@ -77,10 +87,9 @@ class CreateUserCommand extends Command {
         });
         $plaintextPassword = $io->askQuestion($questionPassword);
         $user = new User();
-        $hashedPassword = $this->passwordHasher->hashPassword($user, $plaintextPassword);
         $user
             ->setEmail($email)
-            ->setPassword($hashedPassword);
+            ->setPassword($this->passwordHasher->hashPassword($user, $plaintextPassword));
 
         $this->em->persist($user);
         $this->em->flush();
