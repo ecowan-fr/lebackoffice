69bdd9c44ead7b2b87f29bcf6c2d27916dd8850f
Tydoo Bauer <thomas@tydoo.fr>
2022-03-04 01:20:34 +0100

    ADD terms message

--- src/EventSubscriber/LicenceSubscriber.php
+++ src/EventSubscriber/LicenceSubscriber.php
@@ -8,6 +8,12 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * LA MODIFICATION DE CETTE CLASSE AFIN D'AVOIR ACCESS AUX SERVICES PRÉSENTS
+ * UNIQUEMENT DANS L'ÉDITION PREMIUM, EST PROHIBÉE ET RÉPRÉHENSIBLE AU VU DE LA LOI
+ * EN VERTUE DES ARTICLES 5 ET ARTICLES 6 DES CONDITIONS GENERALES D'UTILISATION
+ * @see https://github.com/ecowan-fr/lebackoffice/blob/main/terms.md
+ */
 class LicenceSubscriber implements EventSubscriberInterface {
 
     public function __construct(
