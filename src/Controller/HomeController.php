948898baeb40b7b560ff1be755539ce13a2ce5ee
Tydoo Bauer <thomas@tydoo.fr>
2022-03-04 01:02:20 +0100

    update security

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
-    #[Route('/', name: 'home.index')]
+    #[Route('/', name: 'home.index', methods: ['GET'])]
     public function index(): RedirectResponse {
         return $this->redirectToRoute('home.home');
     }
 
-    #[Route('/home', name: 'home.home')]
+    #[Route('/home', name: 'home.home', methods: ['GET'])]
     public function home(): Response {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
