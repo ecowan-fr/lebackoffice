1c36c7268a77443cdbe9d63b7b16e20476e6c103
BOYER Thomas <thomas@tydoo.fr>
2022-03-10 03:07:32 +0100

    Update affichage lightbox Welcome

--- assets/js/lightboxWelcome.js
+++ assets/js/lightboxWelcome.js
@@ -1,31 +1,12 @@
 export default (function () {
-
-    global.createCookieWelcome = () => {
-        fetch("/api/cookies/welcome", {
-            method: 'POST',
-            headers: new Headers({
-                "Content-Type": "application/json"
-            })
-        }).then((response) => {
-            if (response.status != 201) {
-                throw new Error("HTTP status " + response.status);
-            }
-            return response.json();
-        })
+    document.createCookieWelcome = () => {
+        localStorage.setItem('lightboxWelcome', "yes");
     }
 
     document.addEventListener("turbo:load", function () {
         //Affichage du message de bienvenue
-        fetch("/api/cookies/welcome", {
-            method: 'GET',
-            headers: new Headers({
-                "Content-Type": "application/json"
-            })
-        }).then((response) => {
-            if (response.status == 404) {
-                Lightbox.S('welcome')
-            }
-            return response.json();
-        })
+        if (!localStorage.lightboxWelcome) {
+            Lightbox.S('welcome')
+        }
     })
 })()
