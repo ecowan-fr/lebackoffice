0bf314867cb98f4ea7238939314f1327a55ed7ea
Tydoo Bauer <thomas@tydoo.fr>
2022-03-14 09:14:19 +0100

    fix

--- src/Controller/LocaleController.php
+++ src/Controller/LocaleController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LocaleController extends AbstractController {
 
