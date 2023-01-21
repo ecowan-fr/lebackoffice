425dcbed7e0971c44e8ea9355bedb5753e595ffa
BOYER Thomas <thomas@tydoo.fr>
2023-01-21 14:21:56 +0100

    🩹 Fix bugs Api platform

--- src/Entity/FlashMessage.php
+++ src/Entity/FlashMessage.php
@@ -3,33 +3,50 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiFilter;
-use ApiPlatform\Core\Action\NotFoundAction;
 use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-#[ApiResource(operations: [new Get(controller: NotFoundAction::class, read: false, output: false), new Post(uriTemplate: '/flash', openapiContext: ['tags' => ['Internal functioning of the backoffice'], 'summary' => 'Create a flash message', 'description' => ''])])]
+#[ApiResource(
+    operations: [
+        new Post(
+            uriTemplate: '/flash',
+            openapiContext: [
+                'tags' => [
+                    'Internal functioning of the backoffice'
+                ],
+                'summary' => 'Create a flash message',
+                'description' => ''
+            ]
+        )
+    ]
+)]
 class FlashMessage {
     #[ApiProperty(description: 'Type', identifier: true, example: 'success', openapiContext: ['enum' => ['success', error::class, 'info', 'warning']])]
     private string $type;
+
     #[ApiProperty(description: 'Message')]
     private string $message;
+
     #[ApiProperty(description: 'The translation domain', default: 'default')]
     private string $domainTranslation = "default";
+
     public function __construct(string $type, string $message, string $domainTranslation = "default") {
         $this->type = $type;
         $this->message = $message;
         $this->domainTranslation = $domainTranslation;
     }
+
     public function getType(): string {
         return $this->type;
     }
+
     public function getMessage(): string {
         return $this->message;
     }
+
     public function getDomainTranslation(): string {
         return $this->domainTranslation;
     }
--- src/Entity/Lightbox.php
+++ src/Entity/Lightbox.php
@@ -8,20 +8,35 @@ use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiFilter;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-#[ApiResource(operations: [new Get(uriTemplate: '/lightbox/{name}', openapiContext: ['tags' => ['Internal functioning of the backoffice'], 'summary' => 'Retrieves the HTML code from a lightbox', 'description' => '', 'security' => [['bearerAuth' => []]]])])]
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/lightbox/{name}',
+            openapiContext: [
+                'tags' => ['Internal functioning of the backoffice'],
+                'summary' => 'Retrieves the HTML code from a lightbox',
+                'description' => '',
+                'security' => [['bearerAuth' => []]]
+            ]
+        )
+    ]
+)]
 class Lightbox {
     #[ApiProperty(description: 'Lightbox name', identifier: true, example: 'welcome')]
     private string $name;
-    #[ApiProperty(description: 'HTML code from a lightbox', example: '<div class="boxtitle">...<\\/div>
-')]
+
+    #[ApiProperty(description: 'HTML code from a lightbox', example: '<div class="boxtitle">...<\\/div>')]
     private string $html;
+
     public function __construct(string $name, string $html) {
         $this->name = $name;
         $this->html = $html;
     }
+
     public function getName(): string {
         return $this->name;
     }
+
     public function getHtml(): string {
         return $this->html;
     }
