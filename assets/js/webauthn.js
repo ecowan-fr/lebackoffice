1ef0218a409a6821af08a6a120865c7a2be91fbd
thomas BOYER <thomas.boyer@interieur.gouv.fr>
2023-01-25 10:59:20 +0100

    🎨 Upgrade to @simplewebauthn/browser

--- assets/js/webauthn.js
+++ assets/js/webauthn.js
@@ -1,14 +1,4 @@
-import { useLogin, useRegistration } from '@web-auth/webauthn-helper';
-
-const webauthnRegister = useRegistration({
-    actionUrl: '/account/security/webauthn/add',
-    optionsUrl: '/account/security/webauthn/add/options',
-});
-
-const webauthnLogin = useLogin({
-    actionUrl: '/login/webauthn',
-    optionsUrl: '/login/webauthn/options',
-});
+import { browserSupportsWebAuthn, startRegistration, startAuthentication } from '@simplewebauthn/browser';
 
 const sendFlashMessage = async function (type, message, redirectUrl, domain) {
     let body;
@@ -28,74 +18,69 @@ const sendFlashMessage = async function (type, message, redirectUrl, domain) {
     })
 }
 
+const redirectUrl = (element) => {
+    return (element.dataset.redirect == '' || element.dataset.redirect == undefined) ? '/' : element.dataset.redirect
+}
+
+const runWebAuthnCeremoni = async (element, optionUrl, verificationUrl, type) => {
+    if (!browserSupportsWebAuthn()) {
+        sendFlashMessage('error', 'It seems this browser does not support WebAuthn...', redirectUrl(element))
+        return;
+    }
+
+    const option = await fetch(optionUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            attestation: 'direct',
+            authenticatorSelection: {
+                requireResidentKey: true,
+                userVerification: 'required'
+            }
+        }),
+    })
+
+    let dataFromToken
+    try {
+        if (type == 'register') {
+            dataFromToken = await startRegistration(await option.json());
+        } else {
+            dataFromToken = await startAuthentication(await option.json());
+        }
+    } catch (error) {
+        if (error.name === 'InvalidStateError' && type == 'register') {
+            sendFlashMessage('error', 'Error: Authenticator was probably already registered by user', redirectUrl(element))
+        } else {
+            sendFlashMessage('error', error, redirectUrl(element))
+        }
+    }
+
+    const verification = await fetch(verificationUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataFromToken),
+    }).then(data => {
+        if (data.ok && type == 'register') {
+            sendFlashMessage('success', "Added your security token successfully", redirectUrl(element), "account")
+        } else if (data.ok) {
+            Turbo.visit(redirectUrl(element))
+        } else {
+            return data.json()
+        }
+    }).then(error => {
+        sendFlashMessage('error', error.errorMessage, redirectUrl(element))
+    })
+}
+
 export default window.WebAuthn = {
-    register: (element) => {
-        let redirectUrl;
-        if (element.dataset.redirect == '' || element.dataset.redirect == undefined) {
-            redirectUrl = '/'
-        } else {
-            redirectUrl = element.dataset.redirect
-        }
-
-        webauthnRegister({
-            attestation: 'direct',
-            authenticatorSelection: {
-                requireResidentKey: true,
-                userVerification: 'required'
-            }
-        }).then(response => {
-            if (response.status == 'ok') {
-                sendFlashMessage('success', "Added your security token successfully", redirectUrl, "account")
-            } else {
-                sendFlashMessage('error', response.errorMessage, redirectUrl)
-            }
-        }).catch(error => {
-            if (error.toString() === '[object Response]') {
-                return error.json();
-            } else {
-                sendFlashMessage('error', error.toString(), redirectUrl)
-            }
-        }).then(json => {
-            if (json.errorMessage !== undefined) {
-                sendFlashMessage('error', json.errorMessage, redirectUrl)
-            } else {
-                sendFlashMessage('error', 'Error !', redirectUrl)
-            }
-        })
+    register: async (element) => {
+        return await runWebAuthnCeremoni(element, '/account/security/webauthn/add/options', '/account/security/webauthn/add', 'register')
     },
-
-    login: (element) => {
-        let redirectUrl;
-        if (element.dataset.redirect == '' || element.dataset.redirect == undefined) {
-            redirectUrl = '/'
-        } else {
-            redirectUrl = element.dataset.redirect
-        }
-
-        webauthnLogin({
-            attestation: 'direct',
-            authenticatorSelection: {
-                requireResidentKey: true,
-                userVerification: 'required'
-            }
-        }).then(response => {
-            if (response.status == 'ok') {
-                Turbo.visit(redirectUrl)
-            } else {
-                sendFlashMessage('error', response.errorMessage, redirectUrl)
-            }
-        }).catch(error => {
-            if (error.toString() === '[object Response]') {
-                return error.json();
-            } else {
-                sendFlashMessage('error', error.toString(), redirectUrl)
-            }
-        }).then(json => {
-            if (json.errorMessage !== undefined) {
-                sendFlashMessage('error', json.errorMessage, redirectUrl)
-            } else {
-                sendFlashMessage('error', 'Error !', redirectUrl)
-            }
-        })
+    login: async (element) => {
+        return await runWebAuthnCeremoni(element, '/login/webauthn/options', '/login/webauthn', 'login')
     }
 }
