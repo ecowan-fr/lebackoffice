fd6cb6a07ad1f3b7652ba7c35ae9620ea6857503
BOYER Thomas <thomas@tydoo.fr>
2023-01-26 13:19:39 +0100

    🩹 Fix WebAuthn Token without metadata

---
+++ migrations/Version20230126091154.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230126091154 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE public_key_credential_source CHANGE metadata_id metadata_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE public_key_credential_source CHANGE metadata_id metadata_id INT NOT NULL');
+    }
+}
